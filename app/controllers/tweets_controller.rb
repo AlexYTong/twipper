fc54cbcf4cc9dbb9c80d536cdbe645c401f6f5f7
atong01 <alexytong@gmail.com>
2014-11-22 14:46:19 -0500

    added tweets list and link

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,16 +1,18 @@
 class TweetsController < ApplicationController
     def new
         @tweet = Tweet.new
+        @tweets = Tweet.all
     end
 
     def create 
         @tweet = Tweet.create(tweet_params)
+        @tweets = Tweet.all
         flash[:success] = "You have created"
         render 'new'
     end
 
     def update
-        @tweet = Tweet.update(:id, tweet_params)
+  #      @tweet = Tweet.update(, tweet_params)
         flash[:success] = "You have updated"
         render 'new'
     end
